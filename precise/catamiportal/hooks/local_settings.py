eda0aa44bc8a4ae6aa2fc5014ed61b268cfb47a3
Mark Gray <markg@ivec.org>
2013-03-12 11:27:47 +0800

    added local_settings and related script support to debug_install

---
+++ precise/catamiportal/hooks/local_settings.py
@@ -0,0 +1,9 @@
+from catamiPortal.conf.settings.dev import *
+
+STAGING_IMPORT_DIR = 'STAGING_IMPORT_DIR_TEXT'
+
+# web image directory - where compressed deployment images are served from
+IMAGES_ROOT =  'IMAGES_ROOT_TEXT'
+
+# the archive directory where we put original full resolution uncompressed images
+STAGING_ARCHIVE_DIR = 'STAGING_ARCHIVE_DIR_TEXT'
